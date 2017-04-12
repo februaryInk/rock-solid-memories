1df0469e3ee698920a897dd3b23fada7a4e20bc1
Farrah Brink <febrink@gmail.com>
2017-04-12 19:58:25 -0400

    Add prices to product variants.

--- db/seeds/base.rb
+++ db/seeds/base.rb
@@ -12,6 +12,18 @@ def self.match_ids( id_set, i, matched_ids, product )
           :sku => ( [ product.id ] + matched_set ).join( '-' )
         )
         o.save
+        
+        Spree::Price.find_or_initialize_by( :variant_id => o.id ).tap do | p |
+          p.assign_attributes(
+            :amount => [
+              product.master.price,
+              rock_image_value_prices[ o.option_values.where( :option_type_id => Spree::OptionType.find_by( :name => 'rock-image' ).id ).first.name ],
+              rock_size_value_prices[ o.option_values.where( :option_type_id => Spree::OptionType.find_by( :name => 'rock-size' ).id ).first.name ]
+            ].sum,
+            :currency => 'USD'
+          )
+          p.save
+        end
       end
     end
   end
@@ -19,6 +31,22 @@ end
 
 def self.product_variants_with_option_values( option_value_ids, product_id )
   Spree::Variant.joins( :option_value_variants ).where( :product_id => product_id ).where( ( [ 'spree_variants.id IN ( SELECT variant_id FROM spree_option_value_variants WHERE option_value_id = ? )' ] * option_value_ids.count ).join( ' AND ' ), *option_value_ids )
+end
+
+def self.rock_image_value_prices
+  { 
+    'True' => 25.00, 
+    'False' => 0.00 
+  }
+end
+
+def self.rock_size_value_prices
+  {
+    'Small' => 0.00,
+    'Medium' => 25.00,
+    'Large' => 50.00,
+    'Extra Large' => 75.00
+  }
 end
 
 puts 'Creating Option Types...'
