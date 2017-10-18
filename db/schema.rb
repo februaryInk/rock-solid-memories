3faa1b936d840e472b48ac638b620414ed8c238b
Farrah Brink <febrink@gmail.com>
2017-10-17 22:21:15 -0400

    Add fonts to database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609174418) do
+ActiveRecord::Schema.define(version: 20171016193715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -198,6 +198,13 @@ ActiveRecord::Schema.define(version: 20170609174418) do
     t.string   "name"
     t.string   "presentation"
     t.string   "value_type"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "spree_fonts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "presentation"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
